966107d75ab57ab918386063b9e94941c29219b6
Natiely Da Cunha <natiely_15@hotmail.com>
2023-04-02 22:20:29 -0500

    saving files

---
+++ src/main/java/com/mccollege/DaCunhaNatiely/DaCunhaNatielyApplication.java
@@ -0,0 +1,13 @@
+package com.mccollege.DaCunhaNatiely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaCunhaNatielyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaCunhaNatielyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mccollege/DaCunhaNatiely/DaCunhaNatielyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mccollege.DaCunhaNatiely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaCunhaNatielyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
